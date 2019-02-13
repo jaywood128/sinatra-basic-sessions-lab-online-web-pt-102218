94049a31048f3b9e0a803ca6313c935438357b36
Johnathon Wood <jaywood128@gmail.com>
2019-02-13 21:19:03 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,13 +1,13 @@
 require_relative 'config/environment'
 require 'pry'
 class App < Sinatra::Base
-enable :sessions 
+enable :sessions
   get '/' do
     erb :index
   end
 
   post '/checkout' do
-    
+
     erb :checkout
   end
 end
